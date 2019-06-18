42c6be8ffb106fb10d92f9c083501020c84d82c5
Resham Arora <resham.arora@gmail.com>
2019-06-18 02:08:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,10 +19,11 @@ end
 end
 
 def find_the_cheese(array)
+  cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |cheese|
     cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
   end
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
   
   
