becd9132b6e78bb3e349cb33489e841db553a6a5
Resham Arora <resham.arora@gmail.com>
2019-06-18 02:06:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,11 +12,19 @@ def summon_captain_planet(array)
   planeteer_calls
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? do |word|
+    word.length > 4
+end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(array)
+  array.find do |cheese|
+    cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
+  end
+  
 end
+  
+  
+  cheese_types = ["cheddar", "gouda", "camembert"]
+
